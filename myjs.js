6ba63ab7b2f61d9abc07e76a05614b06e86284eb
ryuahintateyama <zm26453@josai.ac.jp>
2026-06-22 12:03:22 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,8 +1,15 @@
-alert('Hello,Osamu!');
+alert('Hello,Ryushin!');
 alert(1+1);
 alert(Math.sqrt(1+1));
 alert(Math.sqrt(2)+Math.sqrt(3));
 alert(1/0);
 alert(0/0);
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
+
+
 
 
